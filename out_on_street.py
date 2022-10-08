9315d68b044151151b024cffdcb1dc8b48302fef
yelamin2 <yelamin2@yahoo.com>
2022-10-08 14:13:12 -0400

    complete first scene

--- out_on_street.py
+++ out_on_street.py
@@ -1,4 +1,4 @@
-# You are wlking down the street it looks just like the one out of your door
+# You are wlaking down the street it looks just like the one out of your door
 # feeling 
 import time
 
@@ -13,12 +13,17 @@ class OutOnStreet():
     def on_street(self):
 
         print("You are out of your house walking on the street it is sunny and warm. The street looks quite you are the only one walking around....  ")
+        time.sleep(3)
         print("Where do you want to go: \n<left \n>right \n^forward")
-        direction = input('').lower()
+        self.direction = input('').lower()
     
-        answer = 'incorrect'
+        self.answer = 'incorrect'
+        self.action_directions()
 
-        while answer == 'incorrect':
+    def action_directions(self):
+
+        while self.answer == 'incorrect':
+            direction = self.direction
 
             if direction == 'left':
                 print('There is a stranded cat on the tree.')
@@ -30,11 +35,7 @@ class OutOnStreet():
                     self.action_ignore()
                 else: 
                     response = input('').lower()
-                    
-                
-                
-            
-            
+                  
             elif direction == "right":
                 print('You see some crowd on the far side of the street')
                 print('Do you want to go and check what is the deal?')
@@ -58,55 +59,75 @@ class OutOnStreet():
                 direction = input('').lower()
                 continue
 
-      
-
-
     def action_help(self):
-        print('Geat choice do you want to \nget a ladder  \nclimp the tree?')
-        response = input('Climp/Ladder ==>>').lower()
-        if response == 'climp':
-            self.action_climp()
+        print('Geat choice do you want to \nget a ladder  \nclimb the tree?')
+        response = input('climb/Ladder ==>>').lower()
+        if response == 'climb':
+            self.action_climb()
         elif response == 'ladder':
             self.action_ladder()
         else:
-            response = input('Climp/Ladder ==>>').lower()
+            response = input('climb/Ladder ==>>').lower()
             self.action_help()
              
 
-    def action_climp(self):
+    def action_climb(self):
         print('Can you reach the cat \nYes  \nNo ')
         response = input('Yes/No ==>>').lower()
         if response == 'yes':
             self.option_color()
         elif response == 'no':
-            self.action_ladder()
+            self.action_not_reached()
         else:
             response = input('Yes/No ==>>').lower()
-            self.action_climp()
+            self.action_climb()
 
-        print('Geat choice do you want to get a ladder or climp the tree?')
-        response3 = input('climp /ladder ==>>')
+    def action_not_reached(self):
+        print('Do you want to get a ladder or go on with your business. ')
+        response = input('Ladder/Go ==>>').lower()
+        if response == 'ladder':
+            self.option_ladder()
+        elif response == 'go':
+            print("You are back in front of your house on the street....  ")
+            print("Where do you want to go:\n>right \n^forward")
+            direction = input('').lower()
+            if direction == "right" or direction == "forward":
+                self.action_directions()
+            else: 
+                print("Where do you want to go:\n>right \n^forward")
+                direction = input('').lower()
+        else:
+            response = input('ladder/go ==>>').lower()
+            self.action_climb()
+
+        print('Great choice do you want to get a ladder or climb the tree?')
+        response3 = input('climb /ladder ==>>')
 
         
 
-    def action3(self):
-        if self.action3 == True:
-            print('This is action 3')
-            actions2= True
+    def action_ladder(self):
+        print('Oh great you saved a cat that was awsome. Did you notice what color is this cat? \nWhite \nOther ')
+        response = input('white/other ==>>').lower()
+        if response == 'white':
+            print('')
+            
+        elif response == 'no':
+            self.action_ladder()
         else:
-            actions2 = False
-            print('you picked yes')
-            return actions2
+            response = input('Yes/No ==>>').lower()
+            self.action_climb()
+    
+    def action_from_left(self):
+        print("You are back in front of your house on the street....  ")
+        print("Where do you want to go:\n>right \n^forward")
+        direction = input('').lower()
+        if direction == "right" or direction == "forward":
+            self.action_directions()
+        else: 
+            print("Where do you want to go:\n>right \n^forward")
+            direction = input('').lower()
+            self.action_from_left()
 
-        print('Geat choice do you want to get a ladder or climp the tree?')
-        response3 = input('climp /ladder ==>>')
-
-        if self.action == True:
-            print('yes it is true')
-            actions2= True
-        else:
-             actions2 = False
-        return actions2
 
 def play_OutOnStreet():
         outOnStreet = OutOnStreet()
