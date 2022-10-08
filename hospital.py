24243df700e229ada7504f0312a1f47c2fb1662a
matthew-woodington <smwoodington@gmail.com>
2022-10-08 12:22:42 -0400

    adds more script to hospital room

--- hospital.py
+++ hospital.py
@@ -1,9 +1,23 @@
 import time
 
 
+class Player():
+    def __init__(self):
+        self.key_fragments = 0
+
+    def add_key(self):
+        self.key_fragments += 1
+
+
+player = Player()
+
+
 class HospitalRoom():
     def __init__(self):
         self.has_key = False
+
+    def get_key_value(self):
+        return self.has_key
 
     def set_key_found(self):
         self.has_key = True
@@ -106,12 +120,12 @@ class HospitalRoom():
         elif choice_center == 'screens':
             self.screens()
         elif choice_center == 'bathroom':
-            pass
+            self.bathroom()
         else:
             print('That command did nothing, try entering left/center/right to progress.')
 
     def bed(self):
-        print('You move closer to the bed and from this distance you are sure there is someone or something under those sheets.')
+        print('You move closer to the bed and from this distance you are sure there is someone or something under the sheets.')
         time.sleep(1)
         print('Do you move the sheet or back away? continue/back')
         choice_bed = input().lower()
@@ -138,7 +152,7 @@ class HospitalRoom():
                     'That command did nothing, try entering left/center/right to progress.')
 
     def screens(self):
-        print('You turn toward the screens used to monitor the patients vitals and status.')
+        print('You turn toward the screens used to monitor vitals and status.')
         time.sleep(1)
         print('Looking at the screens you notice a faint deviation in the line indicating a pulse...')
         time.sleep(1)
@@ -151,12 +165,93 @@ class HospitalRoom():
         else:
             print('That command did nothing, try entering left/center/right to progress.')
 
+    def bathroom(self):
+        print('As you enter the bathroom and evaluate your surroundings you notice you are a few steps out of view of the mirror,')
+        time.sleep(3)
+        print('and to your left you notice a small trash can with something sticking out of the top as if haphazardly tossed in.')
+        time.sleep(3)
+        print('Would you like to step farther into the room and check the mirror or investigate the trash? mirror/trash')
+        choice_bathroom = input().lower()
+        if choice_bathroom == 'mirror':
+            self.mirror()
+        elif choice_bathroom == 'trash':
+            self.trash()
+        else:
+            print('That command did nothing, try entering left/center/right to progress.')
+
+    def mirror(self):
+        print('For some reason you feel a strong urge to fully enter the room and turn to look in the mirror.')
+        time.sleep(3)
+        print('As you turn to look in the mirror you find you are met with a clear view of the bathrom wall behind you and nothing else.')
+        time.sleep(3)
+        print('Your stomach lurches and you slowly raise a hand to check your own sanity. The hand in front of you looks as real as ever,')
+        time.sleep(3)
+        print('yet the mirror still shows an empty room. You panic and scramble back out ov view of the mirror.')
+        time.sleep(3)
+        print('As your heartbeat slows you contemplate if you would like to keep exploring the bathroom or go back. continue/back')
+        choice_mirror = input().lower()
+        if choice_mirror == 'continue':
+            self.trash()
+        elif choice_mirror == 'back':
+            self.main_room_view()
+        else:
+            print('That command did nothing, try entering left/center/right to progress.')
+
+    def trash(self):
+        print('You approach the trash and inspect what seems to be a file similar to those in the storage unit that has been thrown away.')
+        time.sleep(3)
+        print('Should you take the file out of the trash? yes/no')
+        choice_trash = input().lower()
+        if choice_trash == 'yes':
+            print(
+                'You bend down and remove the file from the trash to further inspect it.')
+            time.sleep(3)
+            print('You begin to flip through the file and this time you can make out the name fairly well. You still do not recognize the information fully,')
+            time.sleep(3)
+            print('but something about it seems to click in your mind in a familiar way.')
+            time.sleep(3)
+            print('As you flip further into the file something slides from between the pages and falls to the ground.')
+            time.sleep(3)
+            print('You look down and find an object on the ground that seems to roughly resemble a broken piece of what looks to be some sort of key.')
+            time.sleep(3)
+            print('Do you pick up the key fragment? yes/no')
+            choice_key = input().lower()
+            if choice_key == 'yes':
+                self.exit_room()
+            elif choice_key == 'no':
+                self.main_room_view()
+            else:
+                print(
+                    'That command did nothing, try entering left/center/right to progress.')
+        elif choice_trash == 'no':
+            self.main_room_view()
+        else:
+            print('That command did nothing, try entering left/center/right to progress.')
+
+    def exit_room(self):
+        print('You bend down to pick up the key, as your fingers touch the key a bright light envelops your vision,')
+        time.sleep(3)
+        print('and you feel yourself closing your eyes as everything fades to black.')
+        return
+
+
+hospital_key = False
+
 
 def play_hospital_room():
     hospital_room = HospitalRoom()
+    print(hospital_key)
 
-    hospital_room.room_start()
-    hospital_room.main_room_view()
+    if hospital_key == False:
+        hospital_room.room_start()
+        hospital_room.main_room_view()
+        hospital_key = True
+        print(hospital_key)
+        player.add_key()
+        print(player.key_fragments)
+    elif hospital_key == True:
+        print('This room seems to be locked now, try another room.')
+        return
 
 
 play_hospital_room()
