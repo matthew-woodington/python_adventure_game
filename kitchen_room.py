67598b8b4c8d4c772b6fc9220e93f13e26718cfc
matthew-woodington <smwoodington@gmail.com>
2022-10-09 11:56:08 -0400

    adds control flow to locks

--- kitchen_room.py
+++ kitchen_room.py
@@ -1,53 +1,58 @@
 import time
 
+
 class KitchenRoom():
     def __init__(self):
         self.has_key = False
-    
+
     def set_key_found(self):
         self.has_key = True
 
     def start_kitchen(self):
-        print('You enter a dimly lit hallway just outside of your kitchen, walking foward...')
-        time.sleep(3)
+        print(
+            'You enter a dimly lit hallway just outside of your kitchen, walking foward...')
+        # time.sleep(3)
         print('You enter the kitchen. Your eyes focus on the current time on the stove.')
-        time.sleep(3)
-        print('Your heart sinks as you realize it is 10:45am on a Thursday!') 
-        time.sleep(3)
-        print('Do you not have somewhere to be?') 
-        time.sleep(3)
+        # time.sleep(3)
+        print('Your heart sinks as you realize it is 10:45am on a Thursday!')
+        # time.sleep(3)
+        print('Do you not have somewhere to be?')
+        # time.sleep(3)
         print('In a frantic panic you examine the kitchen.')
-        time.sleep(3)
+        # time.sleep(3)
 
     def start_kitchen_again(self):
-        time.sleep(1)
-        print('Your eyes come to focus and you realize you are somehow back in your kitchen.')
-        time.sleep(3)
+        # time.sleep(1)
+        print(
+            'Your eyes come to focus and you realize you are somehow back in your kitchen.')
+        # time.sleep(3)
         print('You glance back at the stove and see that it is still 10:45am. How is this possible!')
         self.pick_ignore()
-        
+
     def pick_ignore(self):
         print('You notice your phone on the kitchen table. Should you pick it up? Enter pick up or ignore:')
         choice_pick_ignore = input().lower()
-        if choice_pick_ignore =='pick up':
+        if choice_pick_ignore == 'pick up':
             print('You walk over to the kitchen table and pick up your phone.')
             self.pick_up()
         elif choice_pick_ignore == 'ignore':
-            print('You understand that the day is already well under way. Might as well try to enjoy it.')
+            print(
+                'You understand that the day is already well under way. Might as well try to enjoy it.')
             self.ignore()
         else:
             print('ENTER A VALID CHOICE! Pick up or ignore?')
-            
+
     def pick_up(self):
         print('Your notice your phone has no new notifications. Not even an email. Peculiar...')
-        time.sleep(3)
+        # time.sleep(3)
         print('You might as well enjoy your morning. You feel your stomach growl. How about breakfast?')
-        time.sleep(3)    
+        # time.sleep(3)
         print('Would you like to have breakfast? Type Yes or No.')
         pick_up_choices = input().lower()
-        time.sleep(2)
+        # time.sleep(2)
         if pick_up_choices == 'yes':
-            print("A sandwich sounds great. Who says that breakfast has to be breakfast food anyway?")
+            print(
+                "A sandwich sounds great. Who says that breakfast has to be breakfast food anyway?")
             self.breakfast()
         elif pick_up_choices == 'no':
             print('You suppose it is getting close to lunch time. You will wait for now.')
@@ -57,73 +62,76 @@ class KitchenRoom():
             print('ENTER A VALID CHOICE! Choose Yes or No.')
 
     def ignore(self):
-        time.sleep(2)
+        # time.sleep(2)
         print('With your hands on your hips, unsure how to spend your day, you hear the rustling of small feet shuffling around in the kitchen closet.')
-        time.sleep(3)
+        # time.sleep(3)
         print('Would you like to investigate? Choose yes or no:')
         ignore_choices = input().lower()
-        time.sleep(2)
+        # time.sleep(2)
         if ignore_choices == 'yes':
             print('Cautiously, you slowly approach the closet and crack the door open. Stumbling out is a 3 foot tall pink elephant.')
-            time.sleep(3)
+            # time.sleep(3)
             self.investigate_yes()
         elif ignore_choices == 'no':
             print('You feel confused, thinking you must be seeing this, you saunter over to the kitchen table to pick up your phone.')
-            time.sleep(3)
+            # time.sleep(3)
             self.pick_up()
         else:
             ('ENTER A VALID CHOICE! Choose yes or no:')
 
     def investigate_yes(self):
         print('Startled, the elephant yelps and scurries past you, managing to open and exit throught the back door.')
-        time.sleep(3)
+        # time.sleep(3)
         print('Overcome with curiousity, you follow. Unable to see where exactly your new pick acquaintance went, you need to make a decision.')
-        time.sleep(3)
-        print('Would you like to go left into the garden or right along the patio and pool?')
-        time.sleep(2)
+        # time.sleep(3)
+        print(
+            'Would you like to go left into the garden or right along the patio and pool?')
+        # time.sleep(2)
         print('Enter left or right:')
         follow_choices = input().lower()
-        time.sleep(2)
+        # time.sleep(2)
         if follow_choices == 'left':
             print('You take a left turn and walk along the bushes in your garden.')
-            time.sleep(3)
+            # time.sleep(3)
             self.choice_left()
+        # CHOICE RIGHT
 
     def choice_left(self):
         print('About 15 paces ahead you notice what looks like a pink tail poking out from one of the bushes.')
-        time.sleep(3)
+        # time.sleep(3)
         print('The elephant sees you approach. "Hello there! I didn\'t mean to disturb you. I got frightened and my natural instinct is to run off", it says.')
-        time.sleep(4)
+        # time.sleep(4)
         print('"You seem friendly enough. My name is Emerson."')
-        time.sleep(3)
+        # time.sleep(3)
         print('"Given the look on your face", he continues, "I am betting you are questioning your sanity by being addressed by a creature such as myself."')
-        time.sleep(3)
+        # time.sleep(3)
         print('"I would like to help you. But first, let\'s have a little fun!", Emerson exclaims.')
-        time.sleep(3)
+        # time.sleep(3)
         print('"True or false, you never catch a cold going up in an elevator?"')
-        time.sleep(3)
+        # time.sleep(3)
         print('Enter true or false;')
         true_or_false = input().lower()
-        time.sleep(1)
+        # time.sleep(1)
         if true_or_false == 'true':
             print('"Correct!", Emerson exclaims. "You come down with a cold, never up."')
-            time.sleep(3)
+            # time.sleep(3)
             print('"Wasn\'t that fun?", your new pink friend giggles. "Now you are probably wondering what is going on."')
-            time.sleep(2)
+            # time.sleep(2)
             print('"Trust me and take this with you. This will help you out very soon." Emerson vanishes into a cloud of smoke.')
-            time.sleep(3)
-            print('At your feet appears a small metal fragment resembling a portion of a key.')
-            time.sleep(3)
+            # time.sleep(3)
+            print(
+                'At your feet appears a small metal fragment resembling a portion of a key.')
+            # time.sleep(3)
             print('Pick it up when you are ready...')
             self.ready_key()
         elif true_or_false == 'false':
             print('"So sorry! That is incorrect. Goodbye, friend!"')
-            time.sleep(3)
+            # time.sleep(3)
             print('Emerson waves his arms towards you. Almost instantaneously you become severely fatigued and black out.')
             self.start_kitchen_again()
 
     def ready_key(self):
-        time.sleep(2)
+        # time.sleep(2)
         print('Type continue when you are ready:')
         ready_go = input().lower()
         if ready_go == 'continue':
@@ -131,53 +139,53 @@ class KitchenRoom():
             return
         else:
             print('ENTER A VALID CHOICE! Type continue when you are ready.')
-        
+
     def breakfast(self):
-        print('You gather together the ingredients for a glorious sandwich.') 
-        time.sleep(3)
-        print('Your stomach reassures you that bacon would make a fantastic addition.') 
-        time.sleep(3)
-        print('You turn on the stove and slap a few slices of greasy goodness onto a pan.') 
-        time.sleep(3)
+        print('You gather together the ingredients for a glorious sandwich.')
+        # time.sleep(3)
+        print('Your stomach reassures you that bacon would make a fantastic addition.')
+        # time.sleep(3)
+        print('You turn on the stove and slap a few slices of greasy goodness onto a pan.')
+        # time.sleep(3)
         print('Only a few minutes away from glory!')
-        time.sleep(3)   
+        # time.sleep(3)
         print('Unexpectedly pained by a headache, you decide to lay on the couch to close your eyes.')
-        time.sleep(3)  
+        # time.sleep(3)
         print('You fall asleep...')
-        time.sleep(10)
+        # time.sleep(10)
         print('Suddenly you are awakened by the sound of blaring smoke alarms. THE BACON!')
-        time.sleep(3)
+        # time.sleep(3)
         print('You run to your kitchen closet and within is a tiny pink elephant leaning against the fire extinguisher.')
-        time.sleep(3)
+        # time.sleep(3)
         print("It just waved at you. IT'S ALIVE?!")
-        time.sleep(3)
+        # time.sleep(3)
         print('No time to think about this! You grab the fire extinguisher and swiftly diminish the flames. Crisis averted.')
-        time.sleep(5)
+        # time.sleep(5)
         print('As your heart rate begins to slow down you remember the strange thing you saw in the closet next to the fire extinguisher.')
-        time.sleep(3)
+        # time.sleep(3)
         print('Would you like to investigate or go outside for fresh air? Enter investigate/outside:')
         breakfast_choices = input().lower()
-        time.sleep(2)
+        # time.sleep(2)
         if breakfast_choices == 'investigate':
-            print('You walk over to the closet and open the door. There is nothing in here...')
-            time.sleep(3)
+            print(
+                'You walk over to the closet and open the door. There is nothing in here...')
+            # time.sleep(3)
             # go back to a previous function
         elif breakfast_choices == 'outside':
             print("You walk out into the back yard wiping sweat from your brow. You hear what sounds like a child's laughter coming from the bushes.")
-            time.sleep(3)
+            # time.sleep(3)
             print("Emerging is a 3 foot tall pink elephant. Smiling, he says ''You made it! Nice job. Here, take this with you.''")
-            time.sleep(3)
+            # time.sleep(3)
             print('He hands you what appears to be a piece of a key. Your headache suddenly comes back, hurting twice as bad as before.')
-            time.sleep(2)
+            # time.sleep(2)
             print('You become overwhelmingly light-headed. You black out...')
             return
         else:
             print('ENTER A VALID CHOICE! Choose investigate or outside.')
 
     def breakfast_no(self):
-        time.sleep(2)
+        # time.sleep(2)
         breakfast_no_choices == input().lower()
-        
 
 
 # def play_kitchen_room():
--- main.py
+++ main.py
@@ -62,37 +62,37 @@ class Game():
 
         if player.key_fragments == 0:
             print("You find yourself lying flat on your back. You look straight up, but can only see what seems to be endless darkness. You go begin to sit up and try to get your bearings.")
-            time.sleep(3)
+            # time.sleep(3)
             print("All you are able make out in the darkness is the dim glow of light framing an antique looking wood door. As you look around you notice three more identical doors surrounding you, though the one behind you has no light.")
-            time.sleep(3)
+            # time.sleep(3)
             print("As your eyes begin to adjust to the darkness, you can see that you are in a small empty room with pitch black walls that seem to go on forever.")
-            time.sleep(3)
+            # time.sleep(3)
             print(
                 "It appears that your only options for escape are the doors. You decide to approach one.")
-            time.sleep(3)
+            # time.sleep(3)
         elif player.key_fragments == 1:
             print("Your eyes snap open, expecting to see the world around you, but are instead greeted with infinite darkness. Somehow you've returned to the dark room of doors.")
-            time.sleep(3)
+            # time.sleep(3)
             print("You feel cool metal reasting in your palm. You look down to see a small piece of a broken key. It can't do much in it's current state.")
-            time.sleep(3)
+            # time.sleep(3)
             print("You must pick another door to try.")
-            time.sleep(3)
+            # time.sleep(3)
         elif player.key_fragments == 2:
             print("Everything around you fades to black, and you have trouble seeing. After a moment your surroundings begin to take shape.")
-            time.sleep(3)
+            # time.sleep(3)
             print("Once again you find yourself in the pitch black room of doors. 'What is this place? How do I keep ending up here?', you ask yourself as you clutch the now two key fragments tightly.")
-            time.sleep(3)
+            # time.sleep(3)
             print("There is no use staying here. You must continue to try the doors.")
-            time.sleep(3)
+            # time.sleep(3)
         elif player.key_fragments == 3:
             print("You feel a familiar sensation as all around you turns to black. You recognise immediately that you have returned to the room of doors.")
-            time.sleep(3)
+            # time.sleep(3)
             print("You fear that this cycle may go on forever. An infinite loop of confusion and horror. But, you cannot give up. You must venture on.")
-            time.sleep(3)
+            # time.sleep(3)
             print("You notice that the room is just a bit brighter than before. You realize that the door behind you now has a warm glow coming from behind it and the key that was once in pieces has somehow become whole.")
-            time.sleep(3)
+            # time.sleep(3)
             print("Your only choice is to keep moving forward.")
-            time.sleep(3)
+            # time.sleep(3)
 
         while True:
             choice = input(
@@ -104,40 +104,50 @@ class Game():
                         'This door seems to be locked now, you must have to try another.')
                 else:
                     print("You open the door and step through.")
-                    time.sleep(3)
+                    # time.sleep(3)
                     play_hospital_room()
             elif choice == 'right':
-                print("You open the door and step through.")
-                time.sleep(3)
-                play_OutOnStreet()
+                global played_street
+                if played_street == True:
+                    print(
+                        'This door seems to be locked now, you must have to try another.')
+                else:
+                    print("You open the door and step through.")
+                    # time.sleep(3)
+                    play_OutOnStreet()
             elif choice == 'ahead':
-                print("You open the door and step through.")
-                time.sleep(3)
-                {door_three}
+                global played_kitchen
+                if played_kitchen == True:
+                    print(
+                        'This door seems to be locked now, you must have to try another.')
+                else:
+                    print("You open the door and step through.")
+                    # time.sleep(3)
+                    play_kitchen_room()
             elif choice == 'behind':
                 if player.key_fragments < 3:
                     print(
                         "You try to turn the handle but it will not budge. This door is locked.")
-                    time.sleep(3)
+                    # time.sleep(3)
                 else:
                     print("You insert the reformed key into the door and turn the handle. The door swings open and emits a warm, blinding light. You step through.")
-                    time.sleep(3)
+                    # time.sleep(3)
                     print("The darkness you've grown accustomed to vanishes in an instant. You feel overwhelmed as your senses are bombarded with the sudden change. Your eyes are closed and you are hesitant to open them.")
-                    time.sleep(3)
+                    # time.sleep(3)
                     print(
                         "After a moment, you open eyes and discover you are lying in a hospital bed, surrounded by your friends and family.")
-                    time.sleep(3)
+                    # time.sleep(3)
                     print("You ask what happened and they tell you that you were in a terrible accident. They haven't left your side since and could tell that you were fighting to come back to them the whole time.")
-                    time.sleep(3)
+                    # time.sleep(3)
                     print(
                         "Congratulations, you've made it home after piecing your mind back together.")
-                    time.sleep(3)
+                    # time.sleep(3)
                     print("Thanks for playing!")
                     break
                     # return
             else:
                 print("Please enter a valid response.")
-                time.sleep(3)
+                # time.sleep(3)
 
 
 game = Game()
